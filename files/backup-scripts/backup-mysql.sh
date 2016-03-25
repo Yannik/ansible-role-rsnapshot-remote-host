1b547ded9f545cb7ddab4300e1ee14618da4027d
Yannik Sembritzki <yannik@sembritzki.me>
2016-03-25 21:06:21 +0100

    Add option to specify user and password for mysql backup script

--- files/backup-scripts/backup-mysql.sh
+++ files/backup-scripts/backup-mysql.sh
@@ -2,9 +2,29 @@
 
 set -o errexit
 
+mysqldump_params=""
+
+while getopts ":u:p:" opt; do
+  case $opt in
+    u)
+      mysqldump_params="${mysqldump_params}--user ${OPTARG} "
+      ;;
+    p)
+      mysqldump_params="${mysqldump_params}--password=${OPTARG} "
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 mkdir -p /var/rsnapshot-backup/
 cd /var/rsnapshot-backup/
 
-mysqldump --all-databases --lock-tables --routines --events --triggers --force > mysqldump.sql
+mysqldump --all-databases --lock-tables --routines --events --triggers --force ${mysqldump_params} > mysqldump.sql
 rm -f mysqldump.sql.gz
 gzip mysqldump.sql
