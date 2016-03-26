7759f6a1a5852c4a3b760e0952054d676f2a73f9
Yannik Sembritzki <yannik@sembritzki.me>
2016-03-26 09:43:09 +0100

    Add 'test' command to allow testing the ssh connection

--- templates/command-wrapper.sh
+++ templates/command-wrapper.sh
@@ -4,6 +4,8 @@ case "$SSH_ORIGINAL_COMMAND" in
     sudo\ /etc/rsnapshot/backup-scripts/*)
         $SSH_ORIGINAL_COMMAND
         ;;
+    test)
+        ;;
     *)
         sudo /usr/local/bin/rrsync -ro /
         ;;
