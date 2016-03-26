2abbbb9c6bbbf11c44668d384bdd4ece6cab5ebb
Yannik Sembritzki <yannik@sembritzki.me>
2016-03-26 18:46:45 +0100

    Add documentation

--- templates/command-wrapper.sh
+++ templates/command-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Source: http://binblog.info/2008/10/20/openssh-going-flexible-with-forced-commands/
 case "$SSH_ORIGINAL_COMMAND" in
     sudo\ /etc/rsnapshot/backup-scripts/*)
         $SSH_ORIGINAL_COMMAND
