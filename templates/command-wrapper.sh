2d19f634bb7de35da7109d3255a25d0dbc03f0ab
Yannik Sembritzki <yannik@sembritzki.me>
2016-03-27 15:44:02 +0200

    Use exec to run the restricted commands

--- templates/command-wrapper.sh
+++ templates/command-wrapper.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 # Source: http://binblog.info/2008/10/20/openssh-going-flexible-with-forced-commands/
+
+# Use exec to run the commands.
+# `exec echo a && echo b`
+# or
+# `exec echo a; echo b`
+# will only run the first echo.
+# `exec` replaces the current shell with the specified program, so
+# the shell will automatically exit after the command is executed.
 case "$SSH_ORIGINAL_COMMAND" in
     sudo\ /etc/rsnapshot/backup-scripts/*)
-        $SSH_ORIGINAL_COMMAND
+        exec $SSH_ORIGINAL_COMMAND
         ;;
     test)
         ;;
     *)
-        sudo /usr/local/bin/rrsync -ro /
+        exec sudo /usr/local/bin/rrsync -ro /
         ;;
 esac
