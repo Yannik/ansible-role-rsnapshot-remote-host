522ca2428e93b94d90255828c026f2aae12a05f3
Yannik Sembritzki <yannik@sembritzki.me>
2016-04-01 14:17:03 +0200

    Fix slice returning incorrect result for keys without a comment
    The integrated 'slice' filter actually returns a fixed number of slices
    instead of a non-fixed amount of slices with x items. (Which we need
    here)

--- filter_plugins/filter.py
+++ filter_plugins/filter.py
@@ -6,9 +6,19 @@ def split(string, char):
     except Exception, e:
         raise errors.AnsibleFilterError('split plugin error: %s' % str(e) )
 
+def do_slice(value, items_per_slice):
+    seq = list(value)
+    length = len(seq)
+    slices = -(-length // items_per_slice)
+    for slice_number in range(slices):
+        start = slice_number * items_per_slice
+        end = (slice_number + 1) * items_per_slice
+        yield seq[start:end]
+
 class FilterModule(object):
     ''' A filter to split a string into a list. '''
     def filters(self):
         return {
-            'split' : split
+            'split' : split,
+            'slice2': do_slice
         }
