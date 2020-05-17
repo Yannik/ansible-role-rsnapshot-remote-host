7c922108bbba1d0ba65bb640227bde239b90c148
Yannik Sembritzki <yannik@sembritzki.me>
2020-05-17 15:49:37 +0200

    Add gitlab backup script

---
+++ files/backup-scripts/gitlab.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+skip="tar"
+
+while getopts ":s:" opt; do
+  case $opt in
+    s)
+      skip=${OPTARG}
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+. /etc/rsnapshot/backup-scripts/common.sh
+
+create_backup_dir
+
+/opt/gitlab/bin/gitlab-backup create SKIP="$skip" GZIP_RSYNCABLE=yes CRON=1
